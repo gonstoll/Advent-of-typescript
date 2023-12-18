009e4bbb4125ee6b8de0e57adb58fb400f9f5cad
Gonzalo Stoll <gonzalostoll@gonzalo-2.local>
2023-12-18 12:03:28 +0100

    Add another solution to 12

--- 2023/12.ts
+++ 2023/12.ts
@@ -21,3 +21,15 @@ type FindSanta<TList extends Array<any>> = LastItem<TList> extends '🎄'
   : PopTuple<TList> extends infer Rest extends Array<any>
   ? Rest['length']
   : never
+
+// TKdodo's solution, which I think is phenomenal
+// (https://github.com/TkDodo/aot-2023/blob/main/12.ts)
+
+type FindSanta2<
+  TForest extends Array<any>,
+  Acc extends Array<any> = [],
+> = TForest extends [infer Head, ...infer Tail]
+  ? Head extends '🎅🏼'
+    ? Acc['length']
+    : FindSanta2<Tail, [...Acc, Head]>
+  : never
